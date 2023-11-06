77fad73f6db8dffe2026c5b18a2861844b3bb7f7
clickclick-ctrl <clickclick-ctrl@users.noreply.github.com>
2023-11-06 16:31:39 +0900

    Modify Fibonacci.java

--- src/main/java/kr/ac/hansung/Fibonacci.java
+++ src/main/java/kr/ac/hansung/Fibonacci.java
@@ -14,7 +14,7 @@ public class Fibonacci {
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.println("fibonacci(" + i ")=" + fibonacci(i));
+            System.out.println("fibonacci(" + i + ")=" + fibonacci(i));
         }
     }
 
